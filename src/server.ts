ae9df11f72a6f1343355e38777eb92a6109c4810
rabram1 <romanabram@web.de>
2021-05-11 16:44:29 +0200

    ✨ add ask for command feature

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { askForMainPassword } from './utils/questions';
+import { askForMainPassword, chooseCommand } from './utils/questions';
 import { isMainPasswordValid } from './utils/validation';
 
 // function start()
@@ -9,6 +9,17 @@ const start = async () => {
     mainPassword = await askForMainPassword();
   }
   console.log('is valid');
+
+  const command = await chooseCommand();
+
+  switch (command) {
+    case 'list':
+      console.log('List Case');
+      break;
+    case 'add':
+      console.log('Add Case');
+      break;
+  }
 };
 
 start();
--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,4 +1,5 @@
 import inquirer from 'inquirer';
+import { Command } from '../types';
 
 export const askForMainPassword = async (): Promise<string> => {
   const answers = await inquirer.prompt<{ mainPassword: string }>([
@@ -9,4 +10,17 @@ export const askForMainPassword = async (): Promise<string> => {
     },
   ]);
   return answers.mainPassword;
+};
+
+export const chooseCommand = async (): Promise<Command> => {
+  const answers = await inquirer.prompt<{ command: Command }>({
+    type: 'list',
+    name: 'command',
+    message: 'What do you want to do?',
+    choices: [
+      { name: 'List all credentials', value: 'list' },
+      { name: 'Add new credentials', value: 'add' },
+    ],
+  });
+  return answers.command;
 };
