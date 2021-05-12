6347309defcde2108a3e3255d147343c2d079477
rabram1 <romanabram@web.de>
2021-05-12 16:10:16 +0200

    ✨a password hash for main password

--- src/server.ts
+++ src/server.ts
@@ -12,22 +12,14 @@ import {
 } from './utils/validation';
 //function start() {
 const start = async () => {
-  /* Solution with while */
-  // let mainPassword = await askForMainPassword();
-  // while (!isMainPasswordValid(mainPassword)) {
-  //   console.log("is valid");
-  //   mainPassword = await askForMainPassword();
-  // }
-  // console.log("Is valid");
-  /* Solution with recursion */
   const mainPassword = await askForMainPassword();
-  if (!isMainPasswordValid(mainPassword)) {
+  if (!(await isMainPasswordValid(mainPassword))) {
     console.log('Is invalid');
     start();
-    // mainPassword = await askForMainPassword();
-  } else {
-    console.log('Is valid');
+    return;
   }
+  console.log('Is valid');
+
   const command = await askForCommand();
   switch (command) {
     case 'list':
@@ -35,7 +27,7 @@ const start = async () => {
         const service = await chooseService();
         printPassword(service);
       }
-      break; // there is only one valid case, therefore a break stops the process; if more cases may be valid no break is needed
+      break;
     case 'add':
       {
         const startAddCase = async () => {
