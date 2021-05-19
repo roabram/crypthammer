a38b5389ff75c80e6fb506426276b197536d4d25
rabram1 <romanabram@web.de>
2021-05-19 16:21:54 +0200

    ✨Add Save credentials to db via http

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,26 @@ dotenv.config();
 
 import express from 'express';
 import { connectDatabase } from './utils/database';
-import { readCredentials } from './utils/credentials';
+import { readCredentials, saveCredentials } from './utils/credentials';
 
 if (process.env.MONGO_URL === undefined) {
   throw new Error('Missing env MONGO_URL');
 }
 
 const app = express();
-const port = 5000;
+const port = 3333;
+
+app.use(express.json());
 
 app.get('/api/credentials', async (_request, response) => {
   const credentials = await readCredentials();
   response.json(credentials);
 });
 
-app.post('/api/credentials', (_request, response) => {
-  response.send('Add new credential');
+app.post('/api/credentials', (request, response) => {
+  // response.send('Add new credential');
+  response.json(request.body);
+  saveCredentials(request.body);
 });
 
 connectDatabase(process.env.MONGO_URL).then(() => {
